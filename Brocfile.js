9fa26e809c426148c05c0f549679940b2a7376ff
Bryan Veloso <bryan@avalonstar.com>
2015-04-25 11:53:27 -0700

    Add looper to the Brocfile.

--- Brocfile.js
+++ Brocfile.js
@@ -22,10 +22,14 @@ var app = new EmberApp({
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+// Javascripts.
 app.import('bower_components/bounce.js/bounce.min.js');
+app.import('bower_components/looper/src/looper.js');
 app.import('bower_components/moment/moment.js');
 app.import('bower_components/socket.io-client/socket.io.js');
+app.import('vendor/event-notifier.js');
 
-app.import ('vendor/event-notifier.js');
+// Stylesheets.
+app.import('bower_components/looper/src/looper.css');
 
 module.exports = app.toTree();
