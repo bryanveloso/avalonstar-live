c1e2a35c2ded957b0fe5c73e0c88661278d298ed
Bryan Veloso <bryan@avalonstar.com>
2015-05-18 13:06:29 -0700

    Go back to the .js router.

---
+++ app/router.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  // Controls.
+  this.route('dashboard');
+
+  // Overlays.
+  this.route('discussion', {path: 'overlay/discussion'});
+  this.route('gaming', {path: 'overlay/gaming'});
+  this.route('notifier', {path: 'overlay/notifier'});
+  this.route('prologue', {path: 'overlay/prologue'});
+});
+
+export default Router;
