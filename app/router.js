cd58fec06fa5117d5d59bd181cca5eff9bcc1ff6
Bryan Veloso <bryan@avalonstar.com>
2015-11-15 20:45:46 -0800

    Working on the ticker first. :3

--- app/router.js
+++ app/router.js
@@ -8,13 +8,14 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   // Controls.
   this.route('dashboard');
-  this.route('event');
 
   // Overlays.
   this.route('discussion', {path: 'overlay/discussion'});
   this.route('gaming', {path: 'overlay/gaming'});
-  this.route('notifier', {path: 'overlay/notifier'});
   this.route('prologue', {path: 'overlay/prologue'});
+
+  this.route('ticker', {path: 'overlay/ticker'});
+  this.route('notifier', {path: 'overlay/notifier'});
 });
 
 export default Router;
