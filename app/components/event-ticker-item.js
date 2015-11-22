c797b089343d48ee816a5153203adc5ee592dbd5
Bryan Veloso <bryan@avalonstar.com>
2015-11-22 01:22:04 -0800

    Delay the ticker update so the notifier can run (and not overlap).

--- app/components/event-ticker-item.js
+++ app/components/event-ticker-item.js
@@ -9,25 +9,39 @@ export default Ember.Component.extend({
   attributeBindings: ['type:data-event'],
   type: Ember.computed.alias('event.event'),
 
-  didInsertElement() {
+  _addEvent() {
     // A new event here should reset the idle timer of the parent.
     this.get('parentView').send('resetTimer');
-
-    //
-    this.$().removeClass('ticker-item--hidden');
 
     // Store the width of the element in a `data-width` attribute.
     this.$().attr('data-width', this.$().width());
 
-    // If there's a tip, mark it as visible for 10 seconds.
+    // ...
+    // (We should wait a second here.)
+    this.$().removeClass('ticker-item--hidden');
+
+    /*
+      Animation Plan.
+      1) Instantiate off-screen (negative margin) -or- create a 1px wide element.
+      2) Move element on-screen -or- grow element to size of data-width.
+      3) Move in other things.
+    */
+
+    // If the event is a tip, mark it as visible for 10 seconds.
     let tickerMessage = this.$('.ticker__message');
     let toggleClass = 'ticker__message--hidden';
 
-    let delay = 1000 * 60 * 1;
+    let messageDelay = 1000 * 60 * 1;
     tickerMessage.removeClass(toggleClass);
     Ember.run.later((function() {
       tickerMessage.addClass(toggleClass);
-    }), delay);
+    }), messageDelay);
+  },
+
+  didInsertElement() {
+    // This delay is determined by the length of a normal notifier.
+    let delay = 1000 * 8;  // 8 seconds.
+    Ember.run.later((() => { this._addEvent(); }), delay);
   },
 
   eventIsTip: function() {
