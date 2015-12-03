e8e199d88508976b220909aa2965ba28387dc450
Bryan Veloso <bryan@avalonstar.com>
2015-12-03 02:03:11 -0800

    Add timestamp to the event model.

--- app/models/event.js
+++ app/models/event.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   event: DS.attr('string'),
   username: DS.attr('string'),
+  timestamp: DS.attr('number'),
 
   // Substreak specific.
   length: DS.attr('number'),
